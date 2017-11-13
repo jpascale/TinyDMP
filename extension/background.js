9236cf95f1f5d6a390fd319053d39ea77e49a4dc
ioji <ioji@chervonagura.com.ar>
2017-11-13 15:20:11 -0300

    handle text

--- extension/background.js
+++ extension/background.js
@@ -7,14 +7,53 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
       }
     });
 
+    getMetadata(changeInfo.url);
+
     getLocalIPs(function (ips) {
-      sendData(user, ips[0], changeInfo.url);
+      // sendData(user, ips[0], changeInfo.url);
     });
   }
 });
 
 function isParsedWebsite(url) {
   return url.indexOf('lanacion.com') > 0 || url.indexOf('infobae.com') > 0 || url.indexOf('clarin.com') > 0;
+}
+
+function getMetadata(url) {
+  if (url.indexOf('lanacion.com') > 0) {
+    var section = document.getElementsByClassName('encabezado')[0].children[0].getAttribute('title');
+    var subsection = document.getElementsByClassName('encabezado')[0].children[1].children[0].textContent;
+    if (!subsection) {
+      var subsection = document.getElementsByClassName('encabezado')[0].children[0].textContent;
+    }
+    var title = document.querySelectorAll('[itemprop="headline"]')[0].textContent;
+    var ps = document.getElementsByTagName('p');
+    var text = '';
+    for (var i = 0, len = ps.length; i < len; i++) {
+      if (ps[i].textContent.length > 60 && !ps[i].textContent.startsWith('Los comentarios publicados')) {
+        text += ps[i].textContent;
+      }
+    }
+  } else if (url.indexOf('clarin.com') > 0) {
+    var section = document.getElementsByClassName('breadcrumb')[0].children[1].children[1].textContent.trim();
+    var subsection = document.getElementsByClassName('volanta')[0].textContent;
+    var ps = document.getElementsByTagName('p');
+    var text = '';
+    for (var i = 0, len = ps.length; i < len; i++) {
+      if (ps[i].textContent.length > 60 && !ps[i].textContent.startsWith('Para comentar') && !ps[i].textContent.startsWith('Registro Propiedad Intelectual')) {
+        text += ps[i].textContent;
+      }
+    }
+  } else if (url.indexOf('infobae.com') > 0) {
+    var section = document.getElementsByClassName('header-label')[0].children[0].textContent;
+    var ps = document.getElementsByTagName('p');
+    var text = '';
+    for (var i = 0, len = ps.length; i < len; i++) {
+      if (ps[i].getAttribute('class') == "element element-paragraph") {
+        text += ps[i].textContent;
+      }
+    }
+  }
 }
 
 function sendData(username, ip, url) {
