cb2ee8d7ec6dbcf583dc8299cb7503f84afc79ef
ioji <ioji@chervonagura.com.ar>
2017-11-14 15:11:51 -0300

    Graphs on user page

--- server/src/controllers/home.ts
+++ server/src/controllers/home.ts
@@ -22,9 +22,50 @@ export let index = (req: Request, res: Response) => {
 export let user = (req: Request, res: Response) => {
   const user = req.params.user;
   Visit.find({ name: user }, function (err: any, visits: any) {
+    const websites: { name: string, count: number }[] = [
+      {
+        name: "lanacion",
+        count: 0
+      },
+      {
+        name: "clarin",
+        count: 0
+      },
+      {
+        name: "infobae",
+        count: 0
+      },
+    ];
+    const categoriesArray: { name: string, count: number }[] = [];
+    const categories: any = {};
+    visits.forEach((visit: any) => {
+      const c = categories[visit.content.category];
+      if (c) {
+        categories[visit.content.category] = c + 1;
+      } else {
+        categories[visit.content.category] = 1;
+      }
+
+      if (visit.url.indexOf("lanacion.com") > 0) {
+        websites[0].count += 1;
+      } else if (visit.url.indexOf("clarin.com") > 0) {
+        websites[1].count += 1;
+      } else if (visit.url.indexOf("infobae.com") > 0) {
+        websites[2].count += 1;
+      }
+    });
+    Object.keys(categories).forEach(element => {
+      categoriesArray.push({
+        name: element,
+        count: categories[element]
+      });
+    });
+
     res.render("user", {
       title: user,
-      visits: visits
+      visits: visits,
+      categories: categoriesArray,
+      websites: websites
     });
   });
 };
