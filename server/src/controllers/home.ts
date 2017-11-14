9af5d062211645416ba7361d9386b02095f18396
ioji <ioji@chervonagura.com.ar>
2017-11-14 11:09:39 -0300

    Home page

--- server/src/controllers/home.ts
+++ server/src/controllers/home.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express";
 const mongoose = require("mongoose");
+const Visit = require("../models/Visit.js");
 
 /**
  * GET /
  * Home page.
  */
 export let index = (req: Request, res: Response) => {
-  res.render("home", {
-    title: "Herolens DMP",
-    ponele: "test"
+  Visit.find({}, function (err: any, visits: any) {
+    res.render("home", {
+      title: "Herolens DMP",
+      visits: visits
+    });
   });
 };
